1a8d7b16a790a454d46b06438e2fcb059d94e1d8
chenwenhao <chen8888hao@163.com>
2021-12-17 17:18:25 +0800

    add .

---
+++ 16.test.js
@@ -0,0 +1,7 @@
+const TIME = require('./dataFormat.js')
+
+const dt = new Date()
+// console.log(dt);
+const newDT=TIME.dataFormat(dt)
+console.log(newDT);
+
---
+++ moment.js
@@ -0,0 +1,5 @@
+const moment = require('moment')
+const dt = moment().format('LLL')
+console.log(dt);
+
+ 
